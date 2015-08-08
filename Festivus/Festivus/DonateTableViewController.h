7510413b5e05ea509450b3a692dae9ce72d398f7
Caleb Hicks <calebhicks@gmail.com>
2015-08-08 17:01:04 -0600

    adds classes and assigns them in interface builder

---
+++ Festivus/Festivus/DonateTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  DonateTableViewController.h
+//  Festivus
+//
+//  Created by Caleb Hicks on 8/8/15.
+//  Copyright © 2015 Alan Barth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DonateTableViewController : UITableViewController
+
+@end
---
+++ Festivus/Festivus/MusicEventDataSource.h
@@ -0,0 +1,13 @@
+//
+//  MusicPerformanceDataSource.h
+//  Festivus
+//
+//  Created by Caleb Hicks on 8/8/15.
+//  Copyright © 2015 Alan Barth. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MusicEventDataSource : NSObject
+
+@end
